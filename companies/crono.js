b9276d958641f8a9eeb0e272ad647221c8e68238
Charlie Weiss <csweiss45@gmail.com>
2022-06-29 13:45:53 -0400

    fix check Top

--- companies/crono.js
+++ companies/crono.js
@@ -7,6 +7,7 @@ async function chrono24(lowP, highP, tPage) {
     console.log("");
     lowest = "-1";
     highest = "-1";
+    var child = 1
     highTable = "";
     lowTable = "";
     var newURL =
@@ -35,15 +36,16 @@ async function chrono24(lowP, highP, tPage) {
         flag = false;
       }
       await lowP.waitForTimeout(1000);
+      await checkTop(lowP, child);
       lowest = await utilFunc.getItem(
         lowP,
-        "#wt-watches > div:nth-child(1) > a > div.p-x-2.p-b-2.m-t-auto > div.article-price-container > div.article-price > div > strong"
+        "#wt-watches > div:nth-child("+child+") > a > div.p-x-2.p-b-2.m-t-auto > div.article-price-container > div.article-price > div > strong"
       );
       lowDealerStatus = await utilFunc.getItem(
         lowP,
-        "#wt-watches > div:nth-child(1) > a > div.p-x-2.p-b-2.m-t-auto > div.article-seller-container.media-flex.align-items-end.flex-grow > div.media-flex-body > div.article-seller-name.text-sm"
+        "#wt-watches > div:nth-child("+child+") > a > div.p-x-2.p-b-2.m-t-auto > div.article-seller-container.media-flex.align-items-end.flex-grow > div.media-flex-body > div.article-seller-name.text-sm"
       );
-      await lowP.click("#wt-watches > div:nth-child(1) > a", { delay: 20 });
+      await lowP.click("#wt-watches > div:nth-child("+child+") > a", { delay: 20 });
       await lowP.waitForTimeout(500);
       lowTable = String(
         await utilFunc.getItem(
@@ -61,17 +63,17 @@ async function chrono24(lowP, highP, tPage) {
       await highP.goto(newURL + "&searchorder=11&sortorder=11");
       await highP.waitForTimeout(500);
       //await highP.click("#modal-content > div > a", {delay: 20})
-
+      await checkTop(highP, child);
       highest = await utilFunc.getItem(
         highP,
-        "#wt-watches > div:nth-child(1) > a > div.p-x-2.p-b-2.m-t-auto > div.article-price-container > div.article-price > div > strong"
+        "#wt-watches > div:nth-child("+child+") > a > div.p-x-2.p-b-2.m-t-auto > div.article-price-container > div.article-price > div > strong"
       );
       highDealerStatus = await utilFunc.getItem(
         highP,
-        "#wt-watches > div:nth-child(1) > a > div.p-x-2.p-b-2.m-t-auto > div.article-seller-container.media-flex.align-items-end.flex-grow > div.media-flex-body > div.article-seller-name.text-sm"
+        "#wt-watches > div:nth-child("+child+") > a > div.p-x-2.p-b-2.m-t-auto > div.article-seller-container.media-flex.align-items-end.flex-grow > div.media-flex-body > div.article-seller-name.text-sm"
       );
 
-      await highP.click("#wt-watches > div:nth-child(1) > a", { delay: 20 });
+      await highP.click("#wt-watches > div:nth-child("+child+") > a", { delay: 20 });
 
       await highP.waitForTimeout(500);
 
@@ -106,8 +108,18 @@ async function chrono24(lowP, highP, tPage) {
       tPage.url()
     );
     console.log(w);
-    utilFunc.addToJson(w);
+    //utilFunc.addToJson(w);
   }
 }
+
+// checkTop = async (page, c) => {
+//   while (await page.$$("#wt-watches > div:nth-child("+c+") > a > div.article-image-container > span.article-badge.top.reduced") != [] || page.$$("#wt-watches > div:nth-child("+c+")").then(async (handles) => {
+//     return await (await [handles].getProperty('localName') != "div")
+//   })) {
+//     c++;
+//   }
+//   console.log("Child",c)
+//   // result of this finds the nth-child that does not have the "top" tag.
+// }
 
 module.exports = { chrono24 };
--- index.js
+++ index.js
@@ -65,10 +65,10 @@ async function start() {
   });
 
   // await CandC.crownAndCaliber(lowPage, highPage, testPage);
-  await Bobs.bobs(lowPage, highPage, testPage);
-  await david.davidsw(lowPage, highPage, testPage); 
-  await Bazaar.bazaar(lowPage, highPage, testPage);
-  await ewc.EWC(lowPage, highPage, testPage);
+  // await Bobs.bobs(lowPage, highPage, testPage);
+  // await david.davidsw(lowPage, highPage, testPage); 
+  // await Bazaar.bazaar(lowPage, highPage, testPage);
+  // await ewc.EWC(lowPage, highPage, testPage);
   await chrono.chrono24(lowPage, highPage, testPage);
 
   await browser.close();
