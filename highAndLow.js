fa4f4e78bd9fae4c0f1914d7c813d583d12773a1
Charlie Weiss <csweiss45@gmail.com>
2022-06-30 10:39:59 -0400

    created ways toget max high and low prices for c24

---
+++ highAndLow.js
@@ -0,0 +1,10 @@
+highAndLow = { "116500LN-0001": [1000, 2000], "116500LN-0002": [5000, 10000] };
+
+pushToHighAndLow = (refNum, low, high) => {
+  highAndLow[refNum] = [low, high];
+};
+getHighAndLow = (s) => highAndLow[s];
+
+getDict = () => highAndLow;
+
+module.exports = { getHighAndLow, pushToHighAndLow, getDict };
