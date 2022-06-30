705f926e5afcf6a1d3b2d45564022899965094d0
Charlie Weiss <csweiss45@gmail.com>
2022-06-30 18:04:27 -0400

    cleaning up useless files

--- test5.js
+++
@@ -1,4 +0,0 @@
-lowTable = "Model Name/Number: Daytona 116500LN Serial/Year: Random - 2018 Gender: Men's Movement: Automatic 4130 Case: Stainless steel (40mm) w/ Black monobloc Cerachrom bezel in ceramic, engraved tachymetric scale, inner reflector ring engraved w/ serial number and scratch resistant sapphire crystal Dial: Black w/ Chromalight display w/ small seconds hand at 6 o'clock, 30-minute counter at 3 o'clock and 12-hour counter at 9 o'clock Bracelet: Stainless steel Oyster w/ Folding Oysterlock safety clasp and Easylink 5mm comfort extension link Box & Papers: Rolex box, booklets, hang tag, wallet and Rolex warranty card dated 03/2018 Warranty: This pre-owned Rolex comes with Bob's Watches one year warranty Return Policy: If not 100% satisfied, return watch in 3 days for a full refund. Terms. "
-index1 = lowTable.indexOf("Rolex box")
-
-console.log(lowTable.substring(index1,lowTable.indexOf("Rolex box")))
