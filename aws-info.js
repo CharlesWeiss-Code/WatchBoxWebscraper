f9c38b07639aa774c33c7e9f48220d6e49c63f80
Charlie Weiss <csweiss45@gmail.com>
2022-07-06 10:21:29 -0400

    More cleaning

--- aws-info.js
+++ aws-info.js
@@ -3,7 +3,7 @@ const SECRET = "iDnHzt52ltoAoricn0tulBQROE8FGhIGJuIskZ3W";
 const KEY_ID = "AKIAYZQYZAMRXXBKD57D";
 const REGION = "Virginia us-east-1"
 const BUCKET_NAME = "wb-datascrapes-testing"
-const Account_ID="604568617763"
+const ACCOUNT_ID="604568617763"
 getSecret = () => SECRET
 getKeyID = () => KEY_ID
 getBucketName = () => BUCKET_NAME
