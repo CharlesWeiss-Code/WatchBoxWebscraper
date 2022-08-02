7b51db1c2759d62fd13b51788c6dd04c3d1f503d
Charlie Weiss <csweiss45@gmail.com>
2022-08-02 15:27:30 -0400

    Current

--- index.js
+++ index.js
@@ -110,22 +110,22 @@ async function start() {
   await setupPage(highPage);
   await setupPage(testPage);
 
-  // await Bazaar.bazaar(lowPage, highPage, testPage);
-  // await newPages(browser);
-  // await utilFunc.sendMessage(
-  //   "Completed Bazaar: " + new Date().toLocaleString()
-  // );
-  // await CandC.crownAndCaliber(lowPage, highPage, testPage);
-  // await newPages(browser);
-  // utilFunc.sendMessage("Completed CandC: " + new Date().toLocaleString());
+  await Bazaar.bazaar(lowPage, highPage, testPage);
+  await newPages(browser);
+  await utilFunc.sendMessage(
+    "Completed Bazaar: " + new Date().toLocaleString()
+  );
+  await CandC.crownAndCaliber(lowPage, highPage, testPage);
+  await newPages(browser);
+  utilFunc.sendMessage("Completed CandC: " + new Date().toLocaleString());
 
-  // await ewc.EWC(lowPage, highPage, testPage);
-  // await newPages(browser);
-  // utilFunc.sendMessage("Completed EWC: " + new Date().toLocaleString());
+  await ewc.EWC(lowPage, highPage, testPage);
+  await newPages(browser);
+  utilFunc.sendMessage("Completed EWC: " + new Date().toLocaleString());
 
-  // await david.davidsw(lowPage, highPage, testPage);
-  // await newPages(browser);
-  // utilFunc.sendMessage("Completed DavidSW: " + new Date().toLocaleString());
+  await david.davidsw(lowPage, highPage, testPage);
+  await newPages(browser);
+  utilFunc.sendMessage("Completed DavidSW: " + new Date().toLocaleString());
 
   await Bobs.bobs(lowPage, highPage, testPage);
   await newPages(browser);
